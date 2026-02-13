0c460787803b08822d7b0fdb69d7c1c3e5281b0a
Veremeevskii <vveremeevskii@wowworks.ru>
2026-02-13 10:25:58 +0400

    ci: update use cache

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,22 +1,15 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__ . '/src')
-    ->in(__DIR__ . '/tests');
+    ->in('src')
+    ->in('tests');
 
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
-        'strict_param' => false,
+        'strict_param' => true,
         'declare_strict_types' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports' => true,
-        'not_operator_with_successor_space' => false,
-        'trailing_comma_in_multiline' => true,
-        'linebreak_after_opening_tag' => true,
-        'no_trailing_whitespace' => true,
-        'no_whitespace_in_blank_line' => true,
     ])
     ->setFinder($finder)
-    ->setUsingCache(true);
+    ->setUsingCache(true)
+    ->setCacheFile('.php-cs-fixer.cache');
